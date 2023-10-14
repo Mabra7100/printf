e89e59a52909dd8981f1f0459d31eacf1756c7d9
Sixolise <sixolisemabutiklaas@gmail.com>
2023-10-14 11:05:25 +0200

    files

---
+++ _printf.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ *_printf - its printf like function
+ *
+ *@format: Arguments
+ *
+ *return: return chars
+ */
+
+int _printf(const char *format, ...)
+{
+	int chars = 0;
+	va_list ap;
+	va_start(ap, format);
+
+    while (*format != '\0')
+    {
+        if (*format == '%')
+        {
+            format++;
+
+            switch (*format)
+            {
+            case 'c':
+            	{
+            		char temp = (char)va_arg(ap, int);
+            		chars += write(1, &temp, 1);
+				}
+    break;
+
+            case 's':
+                {
+                    char *str = va_arg(ap, char*);
+                    chars += write(1, str, strlen(str));
+                }
+                break;
+            default:
+                chars += write(1, format, 1);
+            }
+        }
+        else
+        {
+            chars += write(1, format, 1);
+        }
+
+        format++;
+    }
+
+    va_end(ap);
+
+    return chars;
+}
