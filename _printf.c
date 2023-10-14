8be58d31a8100bcd66fc8c2c76d377ce5a9bc99a
Sixolise <sixolisemabutiklaas@gmail.com>
2023-10-14 13:19:52 +0200

    BettyStyle

--- _printf.c
+++ _printf.c
@@ -10,44 +10,44 @@
 
 int _printf(const char *format, ...)
 {
-	int chars = 0;
-	va_list ap;
-	va_start(ap, format);
+int chars = 0;
+va_list ap;
+va_start(ap, format);
 
-    while (*format != '\0')
-    {
-        if (*format == '%')
-        {
-            format++;
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
 
-            switch (*format)
-            {
-            case 'c':
-            	{
-            		char temp = (char)va_arg(ap, int);
-            		chars += write(1, &temp, 1);
-				}
-                break;
+switch (*format)
+{
+case 'c':
+{
+char temp = (char)va_arg(ap, int);
+chars += write(1, &temp, 1);
+}
+break;
 
-            case 's':
-                {
-                    char *str = va_arg(ap, char*);
-                    chars += write(1, str, strlen(str));
-                }
-                break;
-            default:
-                chars += write(1, format, 1);
-            }
-        }
-        else
-        {
-            chars += write(1, format, 1);
-        }
+case 's':
+{
+char *str = va_arg(ap, char*);
+chars += write(1, str, strlen(str));
+}
+break;
+default:
+chars += write(1, format, 1);
+}
+}
+else
+{
+chars += write(1, format, 1);
+}
 
-        format++;
-    }
+format++;
+}
 
-    va_end(ap);
+va_end(ap);
 
-    return chars;
+return (chars);
 }
